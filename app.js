256cef8c7c377895d901bffa523e72678213bb97
jfernandez0296 <60338655+jfernandez0296@users.noreply.github.com>
2023-01-06 23:05:56 -0500

    Update app.js

--- app.js
+++ app.js
@@ -25,5 +25,5 @@ app.get("/my-first-endpoint", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}/my-first-endpoint`);
 });
