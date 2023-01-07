4c0d95f0fa6433fc12f16810d12026dacf4a61be
jfernandez0296 <60338655+jfernandez0296@users.noreply.github.com>
2023-01-06 23:02:45 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => {
+app.get("/my-first-endpoint", (req, res) => {
   res.status(200).send({
     message: "Curso encontrado satisfactoriamente",
     top: {
